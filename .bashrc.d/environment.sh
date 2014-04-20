322ee4e8dc48f79355d3234dcb5c192daeab3f17
Dmitry Konishchev <konishchev@gmail.com>
2014-04-20 14:11:24 +0400

    Configure virtualenvwrapper

--- .bashrc.d/environment.sh
+++ .bashrc.d/environment.sh
@@ -13,13 +13,26 @@ if [ "$(uname)" = Darwin ]; then
 
     # Add bin directory of main Python interpreter to $PATH to be able to
     # locate package binaries installed by pip
-    if which python >/dev/null; then
+    if python_path="$(which python)"; then
         python_path="$(python -c '__import__("sys").stdout.write(__import__("sys").prefix)')/bin"
         grep -qxF "$python_path" <<< "${PATH//:/$'\n'}" || PATH="$PATH:$python_path"
-        unset python_path
     fi
+    unset python_path
 elif [ -f /etc/debian_version ]; then
     # Debian package building
     export DEBEMAIL='konishchev@gmail.com'
     export DEBFULLNAME='Dmitry Konishchev'
 fi
+
+# Set up virtualenvwrapper
+if virtualenvwrapper=$(which virtualenvwrapper.sh); then
+    export PROJECT_HOME=~/programs
+
+    if python_path="$(which python3)"; then
+        export VIRTUALENVWRAPPER_PYTHON="$python_path"
+    fi
+    unset python_path
+
+    . "$virtualenvwrapper"
+fi
+unset virtualenvwrapper
