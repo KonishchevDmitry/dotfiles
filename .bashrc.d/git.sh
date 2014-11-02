0cb5724a17d734f6ea1fc3ed4f9d730cec630a9a
Dmitry Konishchev <konishchev@gmail.com>
2014-11-02 17:23:40 +0300

    Fix bash completion for new MacPorts

--- .bashrc.d/git.sh
+++ .bashrc.d/git.sh
@@ -2,8 +2,7 @@
 
 # __git_ps1 is not included to bash completion
 [ "$(uname)" = Darwin ] && ! shopt -oq posix && \
-    [ -f /opt/local/share/git-core/git-prompt.sh ] && \
-        . /opt/local/share/git-core/git-prompt.sh
+    [ -f /opt/local/share/git/git-prompt.sh ] && . /opt/local/share/git/git-prompt.sh
 
 # Show current git branch in the command line prompt
 __git_ps1 > /dev/null 2>&1 && PS1+='$(__git_ps1 ":%s")'
