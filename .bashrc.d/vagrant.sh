dd2d53f760506787e8ff99d02eccedbb62c701ff
Dmitry Konishchev <konishchev@gmail.com>
2015-03-13 08:40:13 +0300

    Add a few ssh aliases

--- .bashrc.d/vagrant.sh
+++ .bashrc.d/vagrant.sh
@@ -5,7 +5,7 @@
 export VAGRANT_USER_PREP_SCRIPT=~/scripts/vagrant-user-prep
 
 # ssh to Vagrant's tmux session
-_vagrant_ssh_tmux() {
+vagrant-ssh-tmux() {
     local command="${1:-true}"
 
     vagrant ssh -- -t "
@@ -17,8 +17,5 @@ _vagrant_ssh_tmux() {
     "
 }
 
-# ssh to Vagrant's tmux session
-alias vagrant-ssh-tmux=_vagrant_ssh_tmux
-
 # ssh to Vagrant's tmux session inside of the development virtualenv
-alias vagrant-ssh-virtualenv='_vagrant_ssh_tmux "tmux send-keys \"workon vagrant\" Enter"'
+alias vagrant-ssh-virtualenv='vagrant-ssh-tmux "tmux send-keys \"workon vagrant\" Enter"'
