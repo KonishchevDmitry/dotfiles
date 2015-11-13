e37e072d9f777106f0acc078ebf855eec51c4fad
Dmitry Konishchev <konishchev@gmail.com>
2015-11-13 15:44:41 +0300

    Add dotfiles-git alias

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -8,5 +8,8 @@ alias v='vim'
 # Try to use ack instead of grep
 #alias ms='grep -EHnr'
 
+# git for dotfiles
+dotfiles-git() { git --git-dir ~/.git-dir "$@"; }
+
 # ssh to tmux session
 ssh-tmux() { ssh "$@" -t -- 'tmux has-session 2>/dev/null || tmux new-session -d; exec tmux attach-session'; }
