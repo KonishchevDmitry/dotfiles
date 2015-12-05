ff21efd13848f49e8b451093f6bb971037fd921e
Dmitry Konishchev <konishchev@gmail.com>
2015-12-05 14:13:35 +0300

    Fix git integration

--- .bashrc.d/git.sh
+++ .bashrc.d/git.sh
@@ -1,2 +1,4 @@
 # Show current git branch in the command line prompt
-__git_ps1 > /dev/null 2>&1 && PS1+='$(__git_ps1 ":%s")'
+if [ "$(type -t __git_ps1)" == "function" ]; then
+    PS1+='$(__git_ps1 ":%s")'
+fi
