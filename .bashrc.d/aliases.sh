f047b965750ec154b2b743f096436ea1bd56e492
Dmitry Konishchev <konishchev@gmail.com>
2015-12-12 18:39:32 +0300

    Fix dotfiles-git and add ag alias

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -5,11 +5,10 @@ alias l='ls -lhaF'
 alias g='gvim'
 alias v='vim'
 
-# Try to use ack instead of grep
-#alias ms='grep -EHnr'
+alias ag='ag --color-path 32 --color-line-number "1;38" --color-match "48;5;229"'
 
 # git for dotfiles
-dotfiles-git() { git --git-dir ~/.git-dir "$@"; }
+dotfiles-git() { git --git-dir ~/.git-dir --work-tree ~ "$@"; }
 
 # ssh to tmux session
 ssh-tmux() { ssh "$@" -t -- 'tmux has-session 2>/dev/null || tmux new-session -d; exec tmux attach-session'; }
