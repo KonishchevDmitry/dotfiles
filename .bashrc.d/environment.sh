b2066166324f921d33e80244ba9f32797cb0c4b9
Dmitry Konishchev <konishchev@gmail.com>
2016-11-06 12:06:03 +0300

    Add CFLAGS and LDFLAGS to link against openssl

--- .bashrc.d/environment.sh
+++ .bashrc.d/environment.sh
@@ -10,6 +10,9 @@ if [ "$(uname)" = Darwin ]; then
     # To eliminate problems with locale over ssh
     export LANG=en_US.UTF-8
     export LC_ALL=en_US.UTF-8
+
+    export CFLAGS+=-I/usr/local/opt/openssl/include
+    export LDFLAGS+=-L/usr/local/opt/openssl/lib
 elif [ -f /etc/debian_version ]; then
     # Debian package building
     export DEBEMAIL='konishchev@gmail.com'
@@ -22,6 +25,10 @@ if virtualenvwrapper=$(which virtualenvwrapper.sh); then
     . "$virtualenvwrapper"
 fi
 unset virtualenvwrapper
+
+# Set up Go workspace
+export GOPATH=~/programs/go
+PATH="$PATH:$GOPATH/bin"
 
 # Set up gvm (Go Version Manager)
 [ -f ~/.gvm/scripts/gvm ] && . ~/.gvm/scripts/gvm
