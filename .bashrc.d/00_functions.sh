839eeaeb754754523f84d22eae98876dcb645ac7
Dmitry Konishchev <konishchev@gmail.com>
2017-02-02 10:10:47 +0300

    Fix color scheme detection for iTerm2

--- .bashrc.d/00_functions.sh
+++ .bashrc.d/00_functions.sh
@@ -2,11 +2,20 @@
 __is_light_background() {
     local x fg bg
 
-    stty -echo
-        echo -ne '\e]10;?\a\e]11;?\a'
-        IFS=: read -t 0.1 -d $'\a' x fg
-        IFS=: read -t 0.1 -d $'\a' x bg
-    stty echo
+    if [ -n "$VTE_VERSION" ]; then
+        stty -echo
+            echo -ne '\e]10;?\a\e]11;?\a'
+            IFS=: read -t 0.1 -d $'\a' x fg
+            IFS=: read -t 0.1 -d $'\a' x bg
+        stty echo
 
-    [[ ${fg:0:1} =~ [01234567] ]]
+        [[ ${fg:0:1} =~ [01234567] ]]
+        return
+    elif [ -n "$COLORFGBG" ]; then
+        IFS=';' read fg bg <<< "$COLORFGBG"
+        [ "$fg" -lt "$bg" ]
+        return
+    else
+        return 0
+    fi
 }
