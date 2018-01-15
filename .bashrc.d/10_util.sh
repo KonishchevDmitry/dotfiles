0ae125368cd57519d834fdf1431dd2003e61fbea
Dmitry Konishchev <konishchev@gmail.com>
2018-01-15 23:45:37 +0300

    Support dircolors on macOS

--- .bashrc.d/10_util.sh
+++ .bashrc.d/10_util.sh
@@ -4,7 +4,7 @@
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # Enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
+if which -s dircolors; then
     [ -r ~/.dircolors ] && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 
     alias ls='ls --color=auto'
