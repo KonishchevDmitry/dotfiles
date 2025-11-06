e3f3fe668d446e55a4b56a4ea1a785b4eeb8a82f
Dmitry Konishchev <konishchev@gmail.com>
2025-11-06 22:18:03 +0300

    Adapt to default MacOS shell

--- .bashrc.d/92_git.sh
+++ .bashrc.d/92_git.sh
@@ -1,9 +1,9 @@
 # On Fedora bash completion by default includes only git command completions and doesn't include git-prompt.sh
-if [[ -z "$(type -t __git_ps1)" && -e /usr/share/git-core/contrib/completion/git-prompt.sh ]] then
+if [ -z "$(type -t __git_ps1)" ] && [ -e /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
     . /usr/share/git-core/contrib/completion/git-prompt.sh
 fi
 
 # Show current git branch in the command line prompt
-if [[ "$(type -t __git_ps1)" == "function" ]]; then
+if [ "$(type -t __git_ps1)" == "function" ]; then
     PS1+='$(__git_ps1 ":%s")'
 fi
