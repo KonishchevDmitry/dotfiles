46e0207f492b0c5b3a6982d250df76400027d1bd
Dmitry Konishchev <konishchev@gmail.com>
2026-09-22 09:44:03 +0300

    Deprecate manual iTerm2 integration

--- .bashrc.d/98_shell.sh
+++ .bashrc.d/98_shell.sh
@@ -8,8 +8,3 @@ if [ "$UID" -eq 0 ]; then
     PS1="\[\033[31m\]$PS1"
     PS2="\[\033[31m\]$PS2"
 fi
-
-# Enable iTerm2 shell integration
-if [ "$TERM_PROGRAM" = iTerm.app ]; then
-    . ~/.bashrc.d/.iterm2_shell_integration.sh
-fi
